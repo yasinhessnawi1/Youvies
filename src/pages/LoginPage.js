161ab0b3ece80c55c3817d5aae844c02a3a00427
Yasin hessnawi <yasinhessnawi1@gmail.com>
2024-07-01 19:11:43 +0200

    trying to deploy in netfly instead

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,4 @@
-// src/pages/LoginPage.js
-
+// Purpose: This file contains the login page of the application. It allows the user to enter their username and password to login to the application. If the user is not registered, they can navigate to the register page. If the user is already logged in, they will be redirected to the home page.
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ParticleBackground from '../components/ParticleBackground';
@@ -26,7 +25,7 @@ const LoginPage = () => {
     return (
         <div className="login-page">
             <ParticleBackground />
-            <NameParticles text="Youvies" logoSrc="/logo.png" />
+
             <div className="login-card">
                 <h2>Login</h2>
                 <form onSubmit={handleLogin}>
